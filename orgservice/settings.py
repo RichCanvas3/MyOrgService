c0f0feeb7a81aee6ec9f57e7a6b346b23c606a91
ryanpedersen3 <ryanpeders@gmail.com>
2025-05-07 22:13:53 -0700

    update gitignore

--- orgservice/settings.py
+++ orgservice/settings.py
@@ -130,5 +130,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:3001",
-    "http://localhost:5173"
+    "http://localhost:5173",
+    'https://wallet.myorgwallet.io'
 ]
